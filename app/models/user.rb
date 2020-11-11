409b52ceba7df4db5fbf45bcdfbce614a35160c2
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-11 18:07:54 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :kimonos
+  has_many :obis
+  has_many :items
+
+  validates :nickname, present: true
+  validates :email, uniqueness: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
