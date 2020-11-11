37ac0796a73af4a38ab418b9121945b815d8a997
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-11 19:06:49 +0900

    exampleを記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  
+  describe 'ユーザー新規登録' do
+    it 'nickname、email、password、password_confirmationが存在すれば登録できること' do
+    end
+
+    it 'nicknameが空だと登録できないこと' do
+    end
+
+    it 'emailが空だと登録できないこと' do
+    end
+
+    it 'passwordが空だと登録できないこと' do
+    end
+
+    it 'password_confirmationが空だと登録できないこと' do
+    end
+
+    it 'nicknameが11文字以上だと登録できないこと' do
+    end
+
+    it '同じemailがすでに登録されていると登録できないこと' do
+    end
+
+    it 'emailに@が含まれていないと登録できないこと' do
+    end
+
+    it 'passwordが５文字以下だと登録できないこと' do
+    end
+
+    it 'passwordは半角英数字混合でないと登録されないこと' do
+    end
+
+    it 'passwordが英字のみでは登録できないこと' do
+    end
+
+    it 'passwordが数字のみでは登録できないこと' do
+    end
+
+    it 'passwordとpassword_confirmationが同じでないと登録されないこと' do
+    end
+
+  end
 end
