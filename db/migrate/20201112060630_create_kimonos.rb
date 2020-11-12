65a022a1fcdde735bd614afc4c0a783dddabdb42
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-12 15:19:16 +0900

    カラムを設定

--- db/migrate/20201112060630_create_kimonos.rb
+++ db/migrate/20201112060630_create_kimonos.rb
@@ -1,7 +1,16 @@
 class CreateKimonos < ActiveRecord::Migration[6.0]
   def change
     create_table :kimonos do |t|
-
+      t.integer    :kimono_name_id, null: false
+      t.integer    :kimono_genre_id
+      t.integer    :tpo_id
+      t.text       :color_pattern
+      t.string     :season
+      t.integer    :material_id
+      t.date       :wore_date
+      t.date       :cleaned_date
+      t.text       :memo
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_065148) do
+ActiveRecord::Schema.define(version: 2020_11_12_060630) do
+
+  create_table "kimonos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "kimono_name_id", null: false
+    t.integer "kimono_genre_id"
+    t.integer "tpo_id"
+    t.text "color_pattern"
+    t.string "season"
+    t.integer "material_id"
+    t.date "wore_date"
+    t.date "cleaned_date"
+    t.text "memo"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_kimonos_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +41,5 @@ ActiveRecord::Schema.define(version: 2020_11_11_065148) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "kimonos", "users"
 end
