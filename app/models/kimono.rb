8761e5895478279fb2c70d9efbf2bf2e64ec8d84
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-12 16:54:35 +0900

    tpo,materialをストロングパラメーターに設定

--- app/models/kimono.rb
+++ app/models/kimono.rb
@@ -2,6 +2,8 @@ class Kimono < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :kimono_name
   belongs_to :kimono_genre
+  belongs_to :tpo
+  belongs_to :material
 
   validates :kimono_name_id, numericality: { other_than: 1 }
 end
