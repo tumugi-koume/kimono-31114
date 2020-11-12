901f17de4028e734db2a7ee58eec9384f5d1dada
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-12 15:46:26 +0900

    kimono_nameのバリデーションを設定

--- app/models/kimono.rb
+++ app/models/kimono.rb
@@ -1,4 +1,6 @@
 class Kimono < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :kimono_name
+
+  validates :kimono_name_id, numericality: { other_than: 1 }
 end
