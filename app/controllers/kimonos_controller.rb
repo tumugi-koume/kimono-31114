7f5bc49cbadf293aa78a2082687743bff0d6d660
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-12 15:54:40 +0900

    new,createアクションと、ストロングパラメーター設定

--- app/controllers/kimonos_controller.rb
+++ app/controllers/kimonos_controller.rb
@@ -3,5 +3,22 @@ class KimonosController < ApplicationController
   end
 
   def new
+    @kimono = Kimono.new
   end
+
+  def create
+    @kimono = Kimono.new(kimono_params)
+    if @kimono.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def kimono_params
+    params.require(:kimono).permit(:kimono_name_id)
+  end
+
 end
