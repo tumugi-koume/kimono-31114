c1f79f5838e05f2a50be7d3d2434c3779226d1e1
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-12 16:09:25 +0900

    プルダウン用kimono_genreモデルを作成

---
+++ app/models/kimono_genre.rb
@@ -0,0 +1,10 @@
+class KimonoGenre < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---'},
+    { id: 2, name: '袷'},
+    { id: 3, name: '単衣'},
+    { id: 4, name: '夏物'},
+    { id: 5, name: '夏単衣'},
+    { id: 6, name: 'その他'}
+  ]
+end
