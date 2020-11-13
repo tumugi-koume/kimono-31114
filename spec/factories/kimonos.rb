2f58c5547aa95d49ae60ea153f64c433f5b6b913
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-13 14:23:45 +0900

    FactoryBot作成とexampleの記述

--- spec/factories/kimonos.rb
+++ spec/factories/kimonos.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :kimono do
-    
+    kimono_name_id  { 2 }
+    kimono_genre_id { 3 }
+    tpo_id          { 4 }
+    color_pattern   { "半身桜柄、藤色" }
+    season          { "春、秋" }
+    material_id     { 2 }
+    wore_date       { "2020/12/23"} 
+    cleaned_date    { "2019/04/09" }
+    memo            { "臈纈染め" }
+    association :user
   end
 end
--- spec/models/kimono_spec.rb
+++ spec/models/kimono_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
-
 RSpec.describe Kimono, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @kimono = FactoryBot.build(:kimono)
+  end
+
+  describe '着物登録' do
+    it 'kimono_name_idとimageがあれば保存できること' do
+    end
+
+    it 'kimono_genre_idが空でも保存できること' do
+    end
+
+    it 'tpo_idが空でも保存できること' do
+    end
+
+    it 'color_patternが空でも保存できること' do
+    end
+
+    it 'seasonが空でも保存できること' do
+    end
+
+    it 'material_idが空でも保存できること' do
+    end
+
+    it 'wore_dateが空でも保存できること' do
+    end
+
+    it 'cleaned_dateが空でも保存できること' do
+    end
+
+    it 'memoが空でも保存できること' do
+    end
+
+    it 'kimono_name_idが---だと保存されないこと' do
+    end
+
+    it 'imageがないと保存されないこと' do
+    end
+
+    it 'userが紐づいていないと保存されないこと' do
+    end
+
+  end
 end
