51eddbfa754277461f6604836dbbbdf79ad4d93f
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-16 18:32:13 +0900

    登録画面作成、エラーハンドリング表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
 
   def index
     if user_signed_in? 
-      @items = current_user.obis.order("created_at DESC")
+      @items = current_user.items.order("created_at DESC")
     end
   end
 
@@ -12,11 +12,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(obi_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to items_path
     else
       render :new
     end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name_id, :tpo_id, :material_id, :color_pattern, :season, :wore_date, :cleaned_date, :memo, :image).merge(user_id: current_user.id)
   end
 end
