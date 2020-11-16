5ba62ba037ac66e5252b6814691cfe7f8d837f64
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-16 16:22:50 +0900

    コントローラー修正

--- app/controllers/obis_controller.rb
+++ app/controllers/obis_controller.rb
@@ -1,8 +1,10 @@
 class ObisController < ApplicationController
   before_action :authenticate_user!, only: [:index, :new, :create, :show, :edit]
+  before_action :find_obi, only: [:show, :edit, :update, :destroy, :redirect_root]
+  before_action :redirect_root, only: [:show, :edit, :destroy]
 
   def index
-    if user_signed_in?
+    if user_signed_in? 
       @obis = current_user.obis.order("created_at DESC")
     end
   end
@@ -21,6 +23,16 @@ class ObisController < ApplicationController
   end
 
   private
+
+  def find_obi
+    @obi = Obi.find(params[:id])
+  end
+
+  def redirect_root
+    if current_user != @obi.user
+      redirect_to root_path
+    end
+  end
 
   def obi_params
     params.require(:obi).permit(:obi_name_id, :tpo_id, :material_id, :color_pattern, :season, :wore_date, :cleaned_date, :memo, :image).merge(user_id: current_user.id)
