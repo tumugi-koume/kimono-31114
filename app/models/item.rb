3d8437a1fa9d078415176e4af2bd18692990e51a
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-16 18:01:00 +0900

    バリデーション、アソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_name
+  belongs_to :tpo
+  belongs_to :material
+  belongs_to :user
+  has_one_attached :image
+
+  validates :item_name_id, presence: true, numericality: { other_than: 1 }
+  validates :image,       presence: true
 end
