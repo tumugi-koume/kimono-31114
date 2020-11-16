5c01355ca5d08f9a05dcc834b8fe2d65ba307e0c
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-16 14:23:25 +0900

    カラムを設定

---
+++ spec/helpers/obis_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the ObisHelper. For example:
+#
+# describe ObisHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe ObisHelper, type: :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
