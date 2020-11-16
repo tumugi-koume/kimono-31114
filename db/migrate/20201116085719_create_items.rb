2515cabad21d607254db64941a9fec09dd910b25
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-16 17:59:32 +0900

    カラム設定

--- db/migrate/20201116085719_create_items.rb
+++ db/migrate/20201116085719_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.integer    :item_name_id, null: false
+      t.integer    :tpo_id
+      t.text       :color_pattern
+      t.string     :season
+      t.integer    :material_id
+      t.date       :wore_date
+      t.date       :cleaned_date
+      t.text       :memo
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
