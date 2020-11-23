288c130e8cb0656b0a77299240c7d79caf6e4d42
tumugi-koume <le24.juin@ezweb.ne.jp>
2020-11-23 14:53:02 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to:'kimonos#index'
-  resources :kimonos
-  get 'kimonos/search'
+  resources :kimonos do
+    collection do
+      get 'search'
+    end
+  end
   resources :obis
   resources :items
 end
